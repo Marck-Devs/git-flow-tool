8aa349333cf4170aa4e0c89a24acf61914e4651f
Marck C. Guzman <marck@marck-devs.com>
2022-02-25 16:10:10 +0100

    Feat(libs/git): created the tag delete function

--- libs/git/index.js
+++ libs/git/index.js
@@ -2,5 +2,6 @@ module.exports = {
   BasicBranching : require("./branchs"),
   LogUtils: require("./gitlog"),
   BasicStatus: require("./fileStatus"),
-  Checkout: require("./checkout").checkout
+  Checkout: require("./checkout").checkout,
+  Tags: require("./tagging")
 }
--- libs/git/tagging.js
+++ libs/git/tagging.js
@@ -12,7 +12,18 @@ function create(repo, tag, ref = ""){
   return runCMD(CMD);
 }
 
+/**
+ * Delete a tag
+ * @param {string} repo the repo path
+ * @param {string} tag the tag name
+ * @returns {Promise<string>} The command result
+ */
+ function del(repo, tag){
+  const CMD = `git -C ${repo} tag -D ${tag}`;
+  return runCMD(CMD)
+}
 
 module.exports = {
-  create
+  create,
+  delete: del
 }
